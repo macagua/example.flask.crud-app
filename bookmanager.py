df5c73ad89a1dae287f640bcff4331a9065321d8
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-09-27 19:22:28 -0400

    Configuring Flask-SQLAlchemy

--- bookmanager.py
+++ bookmanager.py
@@ -1,8 +1,20 @@
+import os
+
 from flask import Flask
 from flask import render_template
 from flask import request
 
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
