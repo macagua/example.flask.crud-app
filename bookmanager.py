c565c368f34f1bda4cd8cc820061786aeef11949
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-09-27 19:09:14 -0400

    Creating an input form template

--- bookmanager.py
+++ bookmanager.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "My flask app"
-  
+    return render_template("home.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
 
