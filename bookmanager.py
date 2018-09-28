6dbacc67b6a042479fce15370d8ca6a5926c1a48
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-09-28 06:51:19 -0400

    Updating book titles

--- bookmanager.py
+++ bookmanager.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -30,6 +31,16 @@ def home():
         db.session.commit()
     books = Book.query.all()
     return render_template("home.html", books=books)
+
+
+@app.route("/update", methods=["POST"])
+def update():
+    newtitle = request.form.get("newtitle")
+    oldtitle = request.form.get("oldtitle")
+    book = Book.query.filter_by(title=oldtitle).first()
+    book.title = newtitle
+    db.session.commit()
+    return redirect("/")
 
 
 if __name__ == "__main__":
