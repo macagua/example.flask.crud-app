54afd1f4a65e3d9ed2ea699b1c0206990ee6114e
donedeal-giorgio <giorgio@donedeal.ie>
2020-05-11 10:31:31 +0100

    simple setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='flask-crud-app',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
